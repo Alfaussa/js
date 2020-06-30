77ab038caa89edde27d8fa63cf06a5edb938ade3
Alfiya Yausheva <al.yausheva@gmail.com>
2020-06-30 21:10:28 +0300

    slider task not working

--- slider.js
+++ slider.js
@@ -1,35 +1,21 @@
-const leftBtn = document.querySelector("#left");
-const rightBtn = document.querySelector("#right");
-const itemsList = document.querySelector("#items");
-const computedStyles = window.getComputedStyle(itemsList);
-const  item = document.querySelectorAll(".item");
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const itemsList = document.querySelector(".items");
 
-console.log(item);
-
-const minRight = 0;  
-const preShownItems = 300 / step;
-const maxRight = (items.length - preShownItems) * step;
-const step = 100;
-let currentRight = 0;
-
-itemsList.style.right = currentRight;
-
-right.addEventListener("click", e => {
+const loop = (direction, e) => {
   e.preventDefault();
 
-  if (currentRight < maxRight) {
-    currentRight += step;
-    itemsList.style.right = `${currentRight}px`
+  if (direction == "right") {
+    itemsList.appendChild(itemsList.firstElementChild);
+  } else {
+    itemsList.insertBefore(itemsList.lastElementChild, items.firstElementChild);
   }
-})
+};
 
+right.addEventListener("click", (e) => {
+  loop("right", e);
+});
 
-
-left.addEventListener("click", e => {
-  e.preventDefault();
-
-  if (currentRight < minRight) {
-    currentRight -= step;
-    itemsList.style.right = `${currentRight}px`
-  }
-})
+left.addEventListener("click", (e) => {
+  loop("left", e);
+});
