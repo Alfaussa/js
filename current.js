9a46eb71855b2ca7d1e666e407aa96ffcdd67b66
Alfiya <al.yausheva@gmail.com>
2022-07-16 12:38:17 +0300

    *2

--- current.js
+++ current.js
@@ -1,14 +1,17 @@
-let salaries = { 
-    John: 100, 
-    Ann: 160, 
-    Pete: 130
+let menu = { 
+    width: 200, 
+    height: 300, 
+    title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] == 'number') {
+            obj[key] *= 2;
+            
+        }
+        
+    }
 }
-
-let sum = 0;
-for (let key in salaries) {
-    sum += salaries[key];
-   
-}
-
-console.log(sum);
-
+multiplyNumeric(menu);
+console.log(menu);
