5e91b4914478128a8dddb2b05256c79dda927a18
Alfiya <al.yausheva@gmail.com>
2022-07-16 12:19:25 +0300

    sum

--- current.js
+++ current.js
@@ -1,12 +1,14 @@
-let schedule = {
-    name:"alfiya",
-};
+let salaries = { 
+    John: 100, 
+    Ann: 160, 
+    Pete: 130
+}
 
-function isEmpty (schedule) {
-    for ( let key in schedule) {
-            return false;
-         }
-         return true;
-    }
+let sum = 0;
+for (let key in salaries) {
+    sum += salaries[key];
+   
+}
 
-console.log(isEmpty (schedule));
+console.log(sum);
+
