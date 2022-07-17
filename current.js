5672747443ebf6bac50af9add2d6d4f513fd25af
Alfiya <al.yausheva@gmail.com>
2022-07-17 22:50:41 +0300

    task 6

--- current.js
+++ current.js
@@ -1,8 +1,9 @@
-function checkSpam(str) {
-    let lowerStr = str.toLowerCase();
-    return lowerStr.includes('viagra') || lowerStr.includes('xxx')
+function truncate(str, maxlength) {
+
+    return(str.length > maxlength) ?
+    str.slice(0, maxlength - 1) + '...' : str;
+
 }
 
-console.log(checkSpam('buy viagRA now'));
-console.log(checkSpam('free xxxxx'));
-console.log(checkSpam('innocent rabbit'));
+console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20)); //"Вот, что мне хотело..." 
+console.log(truncate("Всем привет!", 20)); //"Всем привет!"
