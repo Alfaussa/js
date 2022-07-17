647fee2ecaf752603f82dfea3617faf9c03c6b51
Alfiya <al.yausheva@gmail.com>
2022-07-17 13:02:32 +0300

    accumulator

--- current.js
+++ current.js
@@ -1,18 +1,13 @@
-function Calculator() {
-    this.read = function() {
-    this.x = +prompt("x?", '');
-    this.y = +prompt("y?", '');    
-        };
-    
-    this.sum = function() {
-        return this.x + this.y;
-    }
+function Accumulator(startingValue){
 
-    this.mul = function() {
-        return this.x * this.y;
+    this.value = startingValue;
+    this.read = function() {
+    
+        this.value += +prompt("Enter a number?",'');
     }
+ 
 }
-let calculator = new Calculator(); 
-calculator.read();
-alert( "Sum=" + calculator.sum() ); 
-alert( "Mul=" + calculator.mul() );
+let accumulator = new Accumulator(1); // начальное значение 1 
+accumulator.read(); // прибавит ввод prompt к текущему значению
+accumulator.read(); // прибавит ввод prompt к текущему значению 
+alert(accumulator.value); // выведет сумму этих значений
