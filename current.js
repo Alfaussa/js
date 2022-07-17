8bd82ebdc9cd006306e8efdb4f1de34af3a9667a
Alfiya <al.yausheva@gmail.com>
2022-07-17 22:03:28 +0300

    task 4

--- current.js
+++ current.js
@@ -1,34 +1,11 @@
-function randomInteger(min, max) {
 
-    return Math.floor(Math.random() * (max  + 1 - min) + min);
+
+function ucFirst(str)  {
+if(!str) return str;
+
+return str[0].toUpperCase() + str.slice(1);
 
 }
 
 
-
-
-
-console.log( randomInteger(1, 5) ); // 1.2345623452 
-console.log( randomInteger(1, 5) ); // 3.7894332423
-console.log( randomInteger(1, 5) ); // 4.3435234525
-console.log( randomInteger(1, 5) ); // 1.2345623452 
-console.log( randomInteger(1, 5) ); // 3.7894332423
-console.log( randomInteger(1, 5) ); // 4.3435234525
-console.log( randomInteger(1, 5) ); // 1.2345623452 
-console.log( randomInteger(1, 5) ); // 3.7894332423
-console.log( randomInteger(1, 5) ); // 4.3435234525
-console.log( randomInteger(1, 5) ); // 1.2345623452 
-console.log( randomInteger(1, 5) ); // 3.7894332423
-console.log( randomInteger(1, 5) ); // 4.3435234525
-console.log( randomInteger(1, 5) ); // 1.2345623452 
-console.log( randomInteger(1, 5) ); // 3.7894332423
-console.log( randomInteger(1, 5) ); // 4.3435234525
-console.log( randomInteger(1, 5) ); // 1.2345623452 
-console.log( randomInteger(1, 5) ); // 3.7894332423
-console.log( randomInteger(1, 5) ); // 4.3435234525
-console.log( randomInteger(1, 5) ); // 1.2345623452 
-console.log( randomInteger(1, 5) ); // 3.7894332423
-console.log( randomInteger(1, 5) ); // 4.3435234525
-console.log( randomInteger(1, 5) ); // 1.2345623452 
-console.log( randomInteger(1, 5) ); // 3.7894332423
-console.log( randomInteger(1, 5) ); // 4.3435234525
+console.log(ucFirst("") );
