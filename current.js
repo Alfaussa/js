2d1bcad41666efb6b1304e8d1d1e57a8079ce761
Alfiya <al.yausheva@gmail.com>
2022-07-20 16:39:02 +0300

    reverce

--- current.js
+++ current.js
@@ -1,18 +1,5 @@
+let arr = [5, 2, 1, -10, 8];
 
+arr.sort((a, b) => b - a);
 
-function filterRangeInPlace(arr, a, b) {
-
- for (let i = 0; i < arr.length; i++) {
-    let item = arr[i];
-
-    if (item < a || item > b){
-        arr.splice(i, 1);
-        i--;
-      
-
- }
-}
-}
-let arr = [5, 3, 8, 1];
-console.log(filterRangeInPlace(arr, 1, 4));
 console.log(arr);
