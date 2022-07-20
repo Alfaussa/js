595136c82b2042b4c627eeb245f5772d07230684
Alfiya <al.yausheva@gmail.com>
2022-07-20 15:59:52 +0300

    task 3

--- current.js
+++ current.js
@@ -1,11 +1,18 @@
 
 
-function filterRange(arr, a, b) {
+function filterRangeInPlace(arr, a, b) {
 
-   return arr.filter((item) => item >= a && item < b )
+ for (let i = 0; i < arr.length; i++) {
+    let item = arr[i];
 
+    if (item < a || item > b){
+        arr.splice(i, 1);
+        i--;
+      
 
+ }
+}
 }
 let arr = [5, 3, 8, 1];
-console.log(filterRange(arr, 1, 4));
+console.log(filterRangeInPlace(arr, 1, 4));
 console.log(arr);
