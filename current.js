444db88f7bba64626f425727efa1ed76cb112f74
Alfiya <al.yausheva@gmail.com>
2022-07-20 23:08:42 +0300

    task calculator

--- current.js
+++ current.js
@@ -1,9 +1,31 @@
-function copySorted(arr) {
+function Calculator() {
 
- return arr.slice().sort();
-    
-}
+    this.methods = {
+        "-": (a, b) => a - b,
+        "+": (a, b) => a + b
+        };
+   this.calculate = function(str) {
 
-let arr = ["HTML", "JavaScript", "CSS"]; 
-console.log(copySorted(arr));
-console.log(arr);
+    let split = str.split(' '),
+        a = +split[0],
+        op = split[1],
+        b = +split[2]
+
+if (!this.methods[op] || isNaN(a) || isNaN(b)){
+    return NaN;
+} else { return this.methods[op](a,b)}
+   };
+   this.addMethod = function(name, func){
+
+    this.methods[name] = func;
+
+}}
+let calc = new Calculator;
+let str = "3 + 7";
+console.log( calc.calculate(str) );
+let powerCalc = new Calculator; 
+powerCalc.addMethod("*", (a, b) => a * b); 
+powerCalc.addMethod("/", (a, b) => a / b); 
+powerCalc.addMethod("**", (a, b) => a ** b);
+let result = powerCalc.calculate("2 ** 3");
+console.log( result ); // 8
