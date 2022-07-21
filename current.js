93a0a27686063f73ee100a019c1956f1aa444f29
Alfiya <al.yausheva@gmail.com>
2022-07-21 13:27:49 +0300

    tsak 2

--- current.js
+++ current.js
@@ -1,12 +1,12 @@
-let vasya = { name: "Вася", age: 25 }; 
-let petya = { name: "Петя", age: 30 }; 
-let masha = { name: "Маша", age: 28 };
-let users = [ vasya, petya, masha ]; 
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 }; 
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
+let users = [ vasya, petya, masha ];
 
 
-let names = users.map(item => item.name);
+let usersMapped = users.map(user => ({
+    fullName:`${user.name} ${user.surname}`, id:`${user.id}`
+}));
 
-
-
-
-console.log( names ); // Вася, Петя, Маша
+console.log( usersMapped[0].id);
+console.log( usersMapped[0].fullName);
